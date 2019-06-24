7c55644f863c0f94b222599050307db4ba268ac8
Kimjammer <john.kim3.1415@gmail.com>
2019-06-24 14:51:02 -0600

    main.js update so it will do something

--- main.js
+++ main.js
@@ -3,5 +3,16 @@ const client = new Discord.Client();
 
 const prefix = "!"
 
+const config = require("./config.json")
+
+client.on("message", (message) => {
+  
+  if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+  if (message.content.startsWith(prefix + "start")) {
+    message.channel.send("Get ready!");
+  }
+});
+
 
 client.login(config.token);
